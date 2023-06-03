d2f8559648b95e51169a9f265a6a5c8e6ae39344
Austin Birt <masao@Austins-MBP.hsd1.mi.comcast.net>
2023-06-03 18:25:50 -0400

    Created Person Class

--- assignmentOne/lab.js
+++ assignmentOne/lab.js
@@ -10,7 +10,60 @@ class Hamster {
     eatFood(){
         console.log('nibble nibble');
     }
-    geetPrice(){
+    getPrice(){
         return this.price
     }
 }
+
+class Person{
+    constructor(name,){
+        this.name=name
+        this.age=0;
+        this.height=0;
+        this.weight=0;
+        this.mood=0;
+        this.hamsters=[];
+        this.bankAccount=0;
+    }
+    getName(){
+        return this.name
+    }
+    getAge(){
+        return this.age;
+    }
+    getWeight(){
+        return this.weight
+    }
+    greet(){
+        console.log(`Hello, my name i ${this.name}. It's very nice to meet you`);
+    }
+    eat(numberOfMeals){
+        this.weight=this.weight+numberOfMeals;
+        this.mood=this.mood+(Math.floor(numberOfMeals/2))
+    }
+    exercise(hoursExercised){
+        this.weight=this.weight-hoursExercised
+    }
+    ageUp(years){
+        this.age=this.age+years;
+        this.height=this.height+(years*1.25);
+        this.weight=this.height+(years*.3)
+        this.mood=this.mood-(years*.25)
+    }
+    buyHamster(hamster){
+        this.hamsters.push(hamster)
+        this.mood=this.mood+10
+        this.bankAccount=this.bankAccount-hamster.getPrice()
+    }
+}
+let hamster1=new Hamster("Squeaks'a'lot")
+let person1=new Person('Austin');
+// person1.eat(3);
+// person1.exercise(3);
+console.log(person1);
+person1.ageUp(5);
+console.log(person1);
+person1.ageUp(3)
+console.log(person1);
+person1.buyHamster(hamster1);
+console.log(person1);
